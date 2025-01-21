69451eabe34bfcd0e96b6258329ce7feb5866d26
Neil Enns <neile@live.com>
2025-01-21 14:04:37 -0800

    Comment out zshrc install

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Set up the gitconfig with ssh set up correctly for use with 1password
 cp ./.gitconfig ~
+
+# # Copy the zsh config
+# cp ./.zshrc ~
+
+# # set zsh as default shell for the remote user
+# chsh -s "$(which zsh)" "$_REMOTE_USER"

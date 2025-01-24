ec1e62b7918da850eb5633fae65553a181aab5fd
Neil Enns <neile@live.com>
2025-01-23 19:08:47 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # Set up the gitconfig with ssh set up correctly for use with 1password
 cp ./.gitconfig ~
 
+# Clone zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Copy the zsh config
 cp ./.zshrc ~
 

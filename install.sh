f5b89a2e540f267780870d29a90bc9cb87edc1d7
Neil Enns <neile@live.com>
2025-02-19 19:36:24 -0800

    Comment out stuff for a bit

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cp ./.gitconfig ~
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Copy the zsh config
-cp ./.zshrc ~
+# cp ./.zshrc ~
 
 # set zsh as default shell for the remote user
-chsh -s "$(which zsh)" "$_REMOTE_USER"
+# chsh -s "$(which zsh)" "$_REMOTE_USER"
